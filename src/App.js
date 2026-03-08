bebb324414a17d921bee5ffdc3c2aef0b44a16e7
Kolia Parasochka <prostokoliadev4@gmail.com>
2026-03-08 08:22:47 +0200

    fixes lang selection for mobile.

--- src/App.js
+++ src/App.js
@@ -10,42 +10,42 @@ import Translate from './translations/translate.json';
 import Projects from './pages/Projects';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-      if (localStorage.getItem('language') == null) {
-        localStorage.setItem('language', 'ua');
-      }
+	constructor(props) {
+		super(props);
+		if (localStorage.getItem('language') == null) {
+			localStorage.setItem('language', 'ua');
+		}
 
-    this.state = {
-      content: Translate[localStorage.getItem('language')]
-    };
+		this.state = {
+			content: Translate[localStorage.getItem('language')]
+		};
 
-    this.setTranslate = this.setTranslate.bind(this);
-  }
-//<Route path="/contacts" element={<Contacts content={this.state.content} />} />
-  render() {
-    return (
-      <div className="App">
-        <Header content={this.state.content} setTranslate={this.setTranslate}/>
-        <div className='wrapper'>
-          <Routes>
-            <Route path="/" element={<Home content={this.state.content} />}/>
-            <Route path="/about" element={<About content={this.state.content} />}/>
-            <Route exact path='*' element={<FourOhFour content={this.state.content}/>}></Route>
-            <Route path="/projects" element={<Projects content={this.state.content} />} />
-          </Routes>
-        </div>
-      </div>
-    ); 
-  }
+		this.setTranslate = this.setTranslate.bind(this);
+	}
+	//<Route path="/contacts" element={<Contacts content={this.state.content} />} />
+	render() {
+		return (
+			<div className="App">
+				<Header content={this.state.content} setTranslate={this.setTranslate} />
+				<div className='wrapper'>
+					<Routes>
+						<Route path="/" element={<Home content={this.state.content} />} />
+						<Route path="/about" element={<About content={this.state.content} />} />
+						<Route exact path='*' element={<FourOhFour content={this.state.content} />}></Route>
+						<Route path="/projects" element={<Projects content={this.state.content} />} />
+					</Routes>
+				</div>
+			</div>
+		);
+	}
 
-  setTranslate(value) {
-    this.setState({
-      content: Translate[value]
-    });
+	setTranslate(value) {
+		this.setState({
+			content: Translate[value]
+		});
 
-    localStorage.setItem('language', value);
-  }
+		localStorage.setItem('language', value);
+	}
 }
 
 export default App;
