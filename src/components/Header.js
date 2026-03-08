78f81f5d0441c17eb843ae07ffa794d85d9fbafe
Kolia Parasochka <prostokoliadev4@gmail.com>
2026-03-08 14:11:52 +0200

    Career - adds translates.

--- src/components/Header.js
+++ src/components/Header.js
@@ -34,7 +34,7 @@ class Header extends React.Component {
                <ul className={this.state.isMobileMenuOpened && this.state.isMobileScreen ? "nav-links nav-links-mobile" : "nav-links nav-links-laptop"}>
                    <li><Link to="/" onClick={() => this.setState({ isMobileMenuOpened: false })}>{this.props.content.homeLink}</Link></li>
                    <li><Link to="/projects" onClick={() => this.setState({ isMobileMenuOpened: false })}>{this.props.content.projectsLink}</Link></li>
-                   <li><Link to="/about" onClick={() => this.setState({ isMobileMenuOpened: false })}>{this.props.content.aboutLink}</Link></li>
+                   <li><Link to="/career" onClick={() => this.setState({ isMobileMenuOpened: false })}>{this.props.content.careerLink}</Link></li>
                    
                    <li>
                         <img className="select-lang-button" src={`https://flagcdn.com/${this.state.flag}.svg`} onClick={() =>  this.setState({ isLangDialogOpened: !this.state.isLangDialogOpened })} width="32" height="20"/> 
--- src/pages/Career/Career.js
+++ src/pages/Career/Career.js
@@ -12,25 +12,25 @@ function Career(props) {
         <div>
             <div id="about" className="about-section">
                 <FadeInSection>
-                    <div className="about-title">{translate.aboutTitle}</div>
+                    <div className="about-title">{translate.careerPage.title}</div>
                     <p className="about-description fade-text">
-                        {translate.about1}      
+                        {translate.careerPage.about1}      
                     </p>
                     <p className="about-description fade-text">
-                        {translate.about2}
+                        {translate.careerPage.about2}
                     </p>
                     <p className="about-description fade-text">
-                        {translate.about3}
+                        {translate.careerPage.about3}
                     </p>
                     <p className="about-description fade-text">
-                        {translate.about4}
+                        {translate.careerPage.about4}
                     </p>
                 </FadeInSection>
             </div>
             <div id="about" className="about-section-ceo">
                 <FadeInSection direction="fade-bottom">
                     <p className="about-description fade-text">
-                        {translate.aboutCEO}      
+                        {translate.careerPage.aboutCEO}      
                     </p>
                 </FadeInSection>
             </div>
